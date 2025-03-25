3bb5723ee83b32ee37662adb11cdaeeb56ae0029
Silas Joisten <silasjoisten@proton.me>
2025-03-25 11:54:15 +0200

    Exercise 3 solutions

--- src/Domain/Entity/Post.php
+++ src/Domain/Entity/Post.php
@@ -10,6 +10,7 @@ use App\Domain\Value\Image;
 use App\Domain\Value\RichText;
 use App\Domain\Value\Slug;
 use App\Domain\Value\Title;
+use Webmozart\Assert\Assert;
 
 final readonly class Post
 {
@@ -24,14 +25,38 @@ final readonly class Post
 
     public function __construct(array $values)
     {
+        Assert::keyExists($values, 'id');
         $this->id = new PostId($values['id']);
+
+        Assert::keyExists($values, 'full_slug');
         $this->slug = new Slug($values['full_slug']);
+
+        Assert::keyExists($values, 'content');
         $values = $values['content'];
+
+        Assert::keyExists($values, 'title');
         $this->title = new Title($values['title']);
+
+        Assert::keyExists($values, 'description');
         $this->description = new Description($values['description']);
-        $this->author = new Author($values['author'][0]);
-        $this->category = new Category($values['category'][0]);
-        $this->content = new RichText($values['content']);
+
+        Assert::keyExists($values, 'image');
+        Assert::keyExists($values['image'], 'filename');
+        Assert::keyExists($values['image'], 'alt');
         $this->image = new Image($values['image']['filename'], $values['image']['alt']);
+
+        Assert::keyExists($values, 'author');
+        Assert::isList($values['author']);
+        Assert::count($values['author'], 1);
+        $this->author = new Author($values['author'][0]);
+
+        Assert::keyExists($values, 'category');
+        Assert::isList($values['category']);
+        Assert::count($values['category'], 1);
+
+        $this->category = new Category($values['category'][0]);
+
+        Assert::keyExists($values, 'content');
+        $this->content = new RichText($values['content']);
     }
 }
--- src/Domain/Value/Slug.php
+++ src/Domain/Value/Slug.php
@@ -4,10 +4,24 @@ declare(strict_types=1);
 
 namespace App\Domain\Value;
 
+use Webmozart\Assert\Assert;
+
 final readonly class Slug
 {
+    /**
+     *  \p{L} → Matches any letter (Latin, Kanji, Hiragana, Katakana, etc.)
+     *  \p{N} → Matches any number (so numbers remain valid in slugs)
+     *  (?:-[\p{L}\p{N}]+)* → Allows hyphenated words (e.g., hello-world)
+     *  (?:\/[\p{L}\p{N}]+(?:-[\p{L}\p{N}]+)*)*\/? → Allows slashes for hierarchical paths
+     *  Trailing slash (\/?) → Optional to allow both /slug and /slug/.
+     */
+    public const string PATTERN = '([\p{L}\p{N}]+(?:-[\p{L}\p{N}]+)*(?:\/[\p{L}\p{N}]+(?:-[\p{L}\p{N}]+)*)*\/?)$';
+
     public function __construct(
         public string $value,
     ) {
+        Assert::stringNotEmpty($value);
+        Assert::notWhitespaceOnly($value);
+        Assert::regex($value, \sprintf('/%s/', self::PATTERN));
     }
 }
