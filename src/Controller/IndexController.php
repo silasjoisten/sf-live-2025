f102bc19fd839adf307ab159e1caa27399ead9f6
Silas Joisten <silasjoisten@proton.me>
2025-03-25 12:11:38 +0200

    Solution exercise 5

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class IndexController extends AbstractController
 {
     #[Route(path: '/', name: 'index')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', [
+            'locale' => $request->getLocale(),
+        ]);
     }
 }
