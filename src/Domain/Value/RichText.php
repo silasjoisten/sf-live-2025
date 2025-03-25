031ad68e9efd992c89f692c756b1f9cecf1e0ed5
Silas Joisten <silasjoisten@proton.me>
2025-03-25 11:54:15 +0200

    Exercise 3 solutions

--- src/Domain/Value/RichText.php
+++ src/Domain/Value/RichText.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace App\Domain\Value;
 
+use Webmozart\Assert\Assert;
+
 final readonly class RichText
 {
     public function __construct(
         public array $values,
     ) {
+        Assert::keyExists($values, 'type');
+        Assert::same($values['type'], 'doc');
+
+        Assert::keyExists($values, 'content');
     }
 }
