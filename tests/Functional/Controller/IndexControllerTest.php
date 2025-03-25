6eddc2e4570bb12caffd35619eadd9d8eba003e7
Silas Joisten <silasjoisten@proton.me>
2025-03-25 12:45:22 +0200

    Tests for exercise 7

--- tests/Functional/Controller/IndexControllerTest.php
+++ tests/Functional/Controller/IndexControllerTest.php
@@ -16,7 +16,50 @@ final class IndexControllerTest extends FunctionalTestCase
     {
         $this->browser()
             ->visit('/')
-            ->expectException(\TypeError::class)
-            ->assertStatus(Response::HTTP_INTERNAL_SERVER_ERROR);
+            ->assertSuccessful()
+            ->assertSee('Welcome to MyBlog');
+    }
+
+    /**
+     * @test
+     */
+    public function unauthenticatedUserOnFirstPageCanAdvance(): void
+    {
+        $this->browser()
+            ->visit('/')
+            ->assertSuccessful()
+            ->assertElementCount('article.card', 8)
+            ->assertSeeElement('span.prev')
+            ->assertNotSeeElement('a.prev')
+            ->assertSeeElement('a.next')
+
+            ->click('a.next')
+            ->assertSuccessful()
+            ->assertOn('/?page=2')
+            ->assertElementCount('article.card', 1)
+            ->assertSeeElement('a.prev')
+            ->assertNotSeeElement('a.next')
+            ->assertSeeElement('span.next');
+    }
+
+    /**
+     * @test
+     */
+    public function unauthenticatedUserOnLastPageCanGoBack(): void
+    {
+        $this->browser()
+            ->visit('/?page=2')
+            ->assertSuccessful()
+            ->assertSee('Welcome to MyBlog')
+            ->assertElementCount('article.card', 1)
+            ->assertSeeElement('a.prev')
+            ->assertNotSeeElement('span.prev')
+            ->assertSeeElement('span.next')
+            ->assertNotSeeElement('a.next')
+
+            ->click('a.prev')
+            ->assertSuccessful()
+            ->assertOn('/?page=1')
+            ->assertElementCount('article.card', 8);
     }
 }
